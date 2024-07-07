ebc969bb672b99a31f61825bdad5ad995d26ace2
atelgammal <a.t.elgammal@gmail.com>
2024-07-07 15:40:21 +0300

    create activator

--- com.TulipTechnologies.parista_robot/src/main/java/com/TulipTechnologies/parista_robot/impl/Activator.java
+++ com.TulipTechnologies.parista_robot/src/main/java/com/TulipTechnologies/parista_robot/impl/Activator.java
@@ -1,16 +1,22 @@
 package com.TulipTechnologies.parista_robot.impl;
-
+// import com.ur.urcap.api.*;
+import com.ur.urcap.api.contribution.ContributionProvider;
+// import com.ur.urcap.api.contribution.installation.InstallationNodeService;
+// import com.ur.urcap.api.contribution.program.ProgramNodeService;
+import com.ur.urcap.api.contribution.startup.StartupNodeService;
+// import com.ur.urcap.api.contribution.ContributionConfiguration;
+// import com.ur.urcap.api.contribution.startup.StartupNodeContribution;
+// import com.ur.urcap.api.domain.data.DataModel;
+// import com.ur.urcap.api.domain.script.ScriptWriter;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
-/**
- * Hello world activator for the OSGi bundle URCAPS contribution
- *
- */
+
 public class Activator implements BundleActivator {
+
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
-		System.out.println("Activator says Hello World!");
+		bundleContext.registerService(StartupNodeService.class, new ClientStartupService(), null);
 	}
 
 	@Override
@@ -18,4 +24,3 @@ public class Activator implements BundleActivator {
 		System.out.println("Activator says Goodbye World!");
 	}
 }
-
