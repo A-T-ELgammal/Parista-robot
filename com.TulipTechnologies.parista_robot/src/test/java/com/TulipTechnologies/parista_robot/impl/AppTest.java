98870a8e7972dd1a465de6bba63c487c30424320
atelgammal <a.t.elgammal@gmail.com>
2024-07-07 10:51:44 +0300

    creating the project structure

---
+++ com.TulipTechnologies.parista_robot/src/test/java/com/TulipTechnologies/parista_robot/impl/AppTest.java
@@ -0,0 +1,19 @@
+package com.TulipTechnologies.parista_robot.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	@Test
+	void testApp() {
+		assertTrue( true );
+	}
+}
