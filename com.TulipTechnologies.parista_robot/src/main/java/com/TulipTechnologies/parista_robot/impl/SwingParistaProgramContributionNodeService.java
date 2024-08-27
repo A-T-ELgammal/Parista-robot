1caa9fbe058249908cf8a430e752e3724cf31701
atelgammal <a.t.elgammal@gmail.com>
2024-08-27 22:18:11 +0300

    excute parista script from contribution node service

--- com.TulipTechnologies.parista_robot/src/main/java/com/TulipTechnologies/parista_robot/impl/SwingParistaProgramContributionNodeService.java
+++ com.TulipTechnologies.parista_robot/src/main/java/com/TulipTechnologies/parista_robot/impl/SwingParistaProgramContributionNodeService.java
@@ -1,5 +1,10 @@
 package com.TulipTechnologies.parista_robot.impl;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import com.ur.urcap.api.contribution.ProgramNodeContribution;
 import com.ur.urcap.api.domain.script.ScriptWriter;
 
@@ -7,32 +12,45 @@ public class SwingParistaProgramContributionNodeService implements ProgramNodeCo
 
     @Override
     public void openView() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'openView'");
+        
     }
 
     @Override
     public void closeView() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'closeView'");
+        
     }
 
     @Override
     public String getTitle() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTitle'");
+        return "Parista-Robot-Cell";
     }
 
     @Override
     public boolean isDefined() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isDefined'");
+        return true;
     }
 
     @Override
     public void generateScript(ScriptWriter writer) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'generateScript'");
+        InputStream is = getClass().getResourceAsStream("/Parista_Robot.urscript");
+        try
+        {
+        if(is != null)
+        {
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while((line = br.readLine()) != null)
+            {
+                writer.appendLine(line);
+            }
+            br.close();
+        }else{
+            writer.generateScript();
+        }
+    }catch(IOException e)
+    {
+        writer.appendLine("textmsg(\"Error reading socketConnection.urscript file: " + e.getMessage() + "\")");
+    }
     }
     
 }
